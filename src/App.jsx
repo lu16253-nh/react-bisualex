35494642e6409ec580f1bc3aaabb8b5e17ddbec5
lu16253-nh <restrepocarvajalluisaf100>
2026-06-22 13:24:04 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import Header from "./componentes/header/Header";
-import Hero from "./componentes/hero/Hero";
+import info from "./componentes/hero/Hero";
 import Info from "./componentes/informacion/Info";
-import Trailer from "./componentes/trailer/Trailer";
-import Comic from "./componentes/comic/Comic";
+import trailer from "./componentes/trailer/Trailer";
+import comic from "./componentes/comic/Comic";
 import Footer from "./componentes/footer/Footer";
+import { LottieLUISAR } from "./componentes/LottieLUISAR";
 
-function App() {
+
+const App = () => {
   return (
-    <>
-      <Header />
-      <Hero />
-      <Info />
-      <Trailer />
-      <Comic />
-      <Footer />   {/* Footer al final */}
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/informacion" element={<Info />} />
+        <Route path="/trailer" element={<Trailer />} />
+        <Route path="/comic" element={<Comic />} />
+        <Route path="/hero" element={<Hero />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/Luisa" element={<LottieLUISAR />} />
+      </Routes>
+    </BrowserRouter>
+  )
+  
 }
 
-export default App;
+export default App
