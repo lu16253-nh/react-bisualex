eba785915925f9929fd95385b78aec1973330d7c
lu16253-nh <restrepocarvajalluisaf100@gmail.com>
2026-06-30 16:40:04 -0500

    rutas configuradas

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,29 @@ import info from "./componentes/hero/Hero";
 import Info from "./componentes/informacion/Info";
 import trailer from "./componentes/trailer/Trailer";
 import comic from "./componentes/comic/Comic";
-import Footer from "./componentes/footer/Footer";
 import { LottieLUISAR } from "./componentes/LottieLUISAR";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Trailer from "./componentes/trailer/Trailer";
 import Comic from "./componentes/comic/Comic";
+import { Footer } from "./componentes/footer/Footer";
 import Hero from "./componentes/hero/Hero";
 import ComicApp from "./componentes/ComicApp";
+import { Home } from "./componentes/Home";
 
 
 const App = () => {
   return (
     <BrowserRouter    >
+      <Header />
       <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="/informacion" element={<Info />} />
-        <Route path="/trailer" element={<Trailer />} />
+        <Route path="/" element={<Home />} />
         <Route path="/comic" element={<Comic />} />
         <Route path="/comicapp" element={<ComicApp />} />
-        <Route path="/hero" element={<Hero />} />
-        <Route path="/footer" element={<Footer />} />
-        <Route path="/luisa" element={<LottieLUISAR />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
-  
+
 }
 
 export default App
---
+++ src/componentes/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "./hero/Hero"
+import Info from "./informacion/Info"
+
+
+export const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <Info/>
+    </>
+  )
+}
